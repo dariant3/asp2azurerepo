ff3f684aca3f716a94e263515a6aa851254f756e
Darian Tichler <dariant3@gmail.com>
2021-10-05 19:19:38 -0700

    1st commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace asp2azure.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
